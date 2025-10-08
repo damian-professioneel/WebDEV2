00b4385574b00f946176e304690882fa72a33406
damian-professioneel <159129862+damian-professioneel@users.noreply.github.com>
2025-10-08 17:22:01 +0200

    Fields

--- my-app/src/Fields.tsx
+++ my-app/src/Fields.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FrontendCSS/styles.css';
+
+type banen =
+{
+    baan: number;
+    spelers: number;
+    speltype: string;
+    occupied: boolean;
+}
+
+const [banenopslag, setBanenopslag] = useState<banen[]>
+([
+    { baan: 1, spelers: 4, speltype: "Padel",  occupied: false },
+    { baan: 2, spelers: 4, speltype: "Padel",  occupied: false },
+    { baan: 3, spelers: 4, speltype: "Padel",  occupied: false },
+    { baan: 4, spelers: 2, speltype: "Padel",  occupied: false },
+    { baan: 5, spelers: 2, speltype: "Padel",  occupied: false },
+    { baan: 6, spelers: 4, speltype: "Tennis", occupied: false },
+    { baan: 7, spelers: 4, speltype: "Tennis", occupied: false },
+    { baan: 8, spelers: 4, speltype: "Tennis", occupied: false },
+    { baan: 9, spelers: 2, speltype: "Tennis", occupied: false },
+    { baan: 10, spelers: 2, speltype: "Tennis", occupied: false }
+])
+
+const deleteBaan = (baan: banen) =>
+{
+    setBanenopslag(prev => prev.filter(b => b.baan != baan.baan));
+}
+
+const loadBaan = banenopslag
+    .filter(baan => baan.occupied != true)
+    .map(baan => <ul>Baan: {baan.baan} - Spelers: {baan.spelers} - Speltype {baan.speltype} <button className="verwijderKnop" onClick={() => deleteBaan(baan)}>Delete</button></ul>
+
+
+)
+
 
 export const FieldsTable: React.FC<{}> = () => {
     return (
@@ -16,6 +51,9 @@ export const FieldsTable: React.FC<{}> = () => {
             </div>
             <button id="knopvooradd">Toevoegen</button>
             <ul id="banenLijst">
+            </ul>
+            <ul id="banenLijst">
+                {loadBaan}
             </ul>
 
         </div>
