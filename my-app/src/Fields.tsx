6caa0b3b8914885c3e059cda3b51fbfa9a1d9a20
damian-professioneel <159129862+damian-professioneel@users.noreply.github.com>
2025-10-08 11:05:05 +0200

    merged joey en damian

--- my-app/src/Fields.tsx
+++ my-app/src/Fields.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './FrontendCSS/styles.css';
 
@@ -22,8 +21,3 @@ export const FieldsTable: React.FC<{}> = () => {
         </div>
     )
 }
-=======
-export const Fields: React.FC = () => {
-    return <div>DimmaD is clueless over react help deze arme jongen....</div>
-};
->>>>>>> 8f0112e15b0258261642ee215ca6444b0c98e19e
